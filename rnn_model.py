d1e2af95f0042a6cefb105be127dd3997bd343a5
Philipp Wirth <pwirth@student.ethz.ch>
2019-07-30 13:15:18 +0200

    first commit

--- rnn_model.py
+++ rnn_model.py
@@ -223,6 +223,7 @@ class TLModel(nn.Module):
                 samples_per_bucket[i] = torch.LongTensor(list(sampler)).cuda() + self.buckets[i]
 
             idxs = self._data2bucket(data, argsort)
+            print(idxs)
             samples = samples_per_bucket[idxs]
             print(samples.size())
             return samples
