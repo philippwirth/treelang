34631d81937085c555b5520c8a81767bf82a7036
Philipp Wirth <pwirth@student.ethz.ch>
2019-07-30 15:08:26 +0200

    first commit

--- rnn_model.py
+++ rnn_model.py
@@ -409,7 +409,7 @@ class TLModel(nn.Module):
             distance = self._apply_bias(distance, self.bias[self.buckets[bucket]:self.buckets[bucket]+bucket_size])
        
             softmaxed = torch.nn.functional.log_softmax(-distance, dim=0) 
-            print(bucket, self.buckets[bucket], data[i], data[argsort[i]])
+            print(bucket, data[i])
             raw_loss = raw_loss - softmaxed[data[argsort[i]] - self.buckets[bucket]].item()
 
             total_loss += raw_loss / data.size(0)
