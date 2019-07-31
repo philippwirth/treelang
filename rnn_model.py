d9cc1d6b9ace17ef23ad9cf78412f229408c284c
Philipp Wirth <pwirth@student.ethz.ch>
2019-07-31 09:53:14 +0200

    first commit

--- rnn_model.py
+++ rnn_model.py
@@ -162,6 +162,7 @@ class TLModel(nn.Module):
 
         data2 = data.clone(); data2[data2 >= 21] = 20#10000] = 9999                      # ugly fix for padding tokens!!
         data2 = torch.cat([(a == d).nonzero() for a, d in zip(argsort,data2)])  # index of data in sorted array
+        print(data2, data2.size())
         mask = None
         for idx in range(0, self.nbuckets):
             partial_mask = data2 >= self.buckets[idx+1]
