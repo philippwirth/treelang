f194a9b5e62b6a01637fdb88e5866566c2faffe9
Philipp Wirth <pwirth@student.ethz.ch>
2019-07-31 09:48:33 +0200

    first commit

--- rnn_model.py
+++ rnn_model.py
@@ -263,6 +263,7 @@ class TLModel(nn.Module):
             x[i] = -d_neg
         
         softmaxed = torch.nn.functional.log_softmax(x, dim=0)
+        print(softmaxed.size(), bucket_idxs.size())
         softmaxed = softmaxed.gather(0, bucket_idxs.view(1,-1))
 
         return softmaxed
@@ -395,7 +396,7 @@ class TLModel(nn.Module):
             bucket_size = self.buckets[bucket+1] - self.buckets[bucket] if bucket < self.nbuckets-1 else self.ntoken - self.buckets[bucket]
             
             softmaxed = torch.nn.functional.log_softmax(-distance, dim=0)
-            #print(left_idx, softmaxed)
+            print(torch.exp(-softmaxed))
             raw_loss = -softmaxed[bucket].item()   # TODOOOO
 
             all_words_times_W_i = all_words_times_W[argsort[i]]
