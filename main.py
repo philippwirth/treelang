0bd97e38c06a98556606d9f215c4f7ff21cc0b59
Philipp Wirth <pwirth@student.ethz.ch>
2019-07-31 12:16:19 +0200

    first commit

--- main.py
+++ main.py
@@ -245,6 +245,8 @@ def run(args, rnn_config, reg_config, threshold_config, sample_config, bucket_co
             _, argsort = torch.sort(log_prob, descending=True)
             argsort = argsort.view(-1, 20)#10000)
 
+            print(argsort.size())
+
             # Starting each batch, we detach the hidden state from how it was previously produced.
             # If we didn't, the model would try backpropagating all the way to start of the dataset. 
             h_tl = tl_model.init_hidden(args.batch_size)
