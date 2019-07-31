b801fbe1a75af4c396908010612934eccc275b56
Philipp Wirth <pwirth@student.ethz.ch>
2019-07-31 14:19:56 +0200

    first commit

--- rnn_model.py
+++ rnn_model.py
@@ -350,6 +350,9 @@ class TLModel(nn.Module):
         samples_emb = self.lockdrop(samples_emb, self.dropouti)
         
         ns_softmaxed = self._logsoftmax_over_neg_samples(d_pos, raw_output, samples, samples_emb)
+        binary2 = torch.ones(ns_softmaxed.size()).cuda().long()
+        binary2[(self._data2bucket(data.view(-1), argsort) < 3).nonzero()] = 1
+        ns_softmaxed = ns_softmaxed * binary2
         #p_per_b = torch.FloatTensor([1., 1., 0.33333, 0.2, 0.1]).cuda()
         #ns_softmaxed = torch.log(p_per_b[self._data2bucket(data.view(-1), argsort)]).view(1,-1)
         print('training -ns_softmax: ' + str(-ns_softmaxed))
