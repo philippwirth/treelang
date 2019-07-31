735f53b2a264b3428a75df9647f592c9a61dc265
Philipp Wirth <pwirth@student.ethz.ch>
2019-07-31 09:51:21 +0200

    first commit

--- rnn_model.py
+++ rnn_model.py
@@ -338,6 +338,7 @@ class TLModel(nn.Module):
         # softmax over tombstones
         tombstones_emb, tombstones_bias = self._get_tombstones(argsort)
         tombstones_emb = self.lockdrop(tombstones_emb, self.dropouti)
+        print(data.size(), data.view(-1).size())
         ts_softmaxed = self._logsoftmax_over_tombstones(self._data2bucket(data.view(-1), argsort), raw_output, tombstones_emb, tombstones_bias) 
         
         # softmax over negative samples
